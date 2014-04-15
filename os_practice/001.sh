c1665424302ab5f6d23b2d9ee9d38f66dfd2391c
catof21 <catof21@gmail.com>
2014-04-15 09:16:58 +0200

    zh feladat

--- os_practice/001.sh
+++ os_practice/001.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+#bash, ami tetszoleges konyvtarakbol kiindulva rekurzivan megszamolja az alkonyvtarakat, szamot ad vissza. 
+#bemeno parameterek (tetszoleges szamu) az egyes konyvtarak nevei
+
+
+while [ $# -ne 0 ] 
+	do
+	echo "${1} ez lesz "
+#	echo $(find "$1" -type d | wc -l)
+	find "$1" -type d | wc -l
+	shift
+	done
+
+echo "vege"
